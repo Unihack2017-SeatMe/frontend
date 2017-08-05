773705b16b7ec9126b31c37a611a8ecb0e069184
Patrick Shaw <Patrick.Leong.Shaw@gmail.com>
2017-08-05 14:36:17 +1000

    Added roomData for socket

--- frontend/src/client/Client.jsx
+++ frontend/src/client/Client.jsx
@@ -1,5 +1,5 @@
 import SocketIo from 'socket.io-client';
-import { allRoomData } from '../../shared/socket-keys';
+import { allRoomData, roomData } from '../../shared/socket-keys';
 function setupSocket(socket, mapState) {
   socket.on(allRoomData, (all_room_data) => {
     mapState.all_room_state = all_room_data;
